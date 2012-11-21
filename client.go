9f8e692ad5867dcddad8c85ce4e8def504b96155
Flavia Missi <flaviamissi@gmail.com>
2012-11-21 11:28:11 -0200

    refactoring NewRepository to use generic method for making requests

--- client.go
+++ client.go
@@ -2,9 +2,10 @@ package gandalfclient
 
 import (
 	"bytes"
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -18,26 +19,34 @@ type repository struct {
 	IsPublic bool     `json:"ispublic"`
 }
 
-func (c *Client) NewRepository(name string, users []string, isPublic bool) (repository, error) {
-    r := repository{Name: name, Users: users, IsPublic: isPublic}
-    j, err := json.Marshal(&r)
-    if err != nil {
-        return repository{}, err
-    }
-	body := bytes.NewBuffer(j)
+func (c *Client) doRequest(method string, body io.Reader) (*http.Response, error) {
 	request, err := http.NewRequest("POST", c.Endpoint, body)
-    if err != nil {
-        return repository{}, err
-    }
-    request.Header.Set("Content-Type", "application/json")
-    response, err := (&http.Client{}).Do(request)
-    if err != nil {
-        return repository{}, err
-    }
-    if response.StatusCode != 200 {
-        b, _ := ioutil.ReadAll(response.Body)
-        msg := fmt.Errorf("Got error while performing request. Code: %s - Message: %s", response.StatusCode, b)
-        return repository{}, msg
-    }
-    return r, nil
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Set("Content-Type", "application/json")
+	response, err := (&http.Client{}).Do(request)
+	if err != nil {
+		return response, err
+	}
+	return response, nil
+}
+
+func (c *Client) NewRepository(name string, users []string, isPublic bool) (repository, error) {
+	r := repository{Name: name, Users: users, IsPublic: isPublic}
+	j, err := json.Marshal(&r)
+	if err != nil {
+		return repository{}, err
+	}
+	body := bytes.NewBuffer(j)
+	response, err := c.doRequest("POST", body)
+	if err != nil {
+		return repository{}, err
+	}
+	if response.StatusCode != 200 {
+		b, _ := ioutil.ReadAll(response.Body)
+		msg := fmt.Errorf("Got error while performing request. Code: %s - Message: %s", response.StatusCode, b)
+		return repository{}, msg
+	}
+	return r, nil
 }
--- client_test.go
+++ client_test.go
@@ -1,9 +1,21 @@
 package gandalfclient
 
 import (
+	"bytes"
 	. "launchpad.net/gocheck"
 	"net/http/httptest"
 )
+
+func (s *S) TestDoRequest(c *C) {
+	h := TestHandler{content: `some return message`}
+	ts := httptest.NewServer(&h)
+	client := Client{Endpoint: ts.URL}
+	body := bytes.NewBufferString(`{"foo":"bar"}`)
+	response, err := client.doRequest("POST", body)
+	c.Assert(err, IsNil)
+	c.Assert(response.StatusCode, Equals, 200)
+	c.Assert(string(h.body), Equals, `{"foo":"bar"}`)
+}
 
 func (s *S) TestNewRepository(c *C) {
 	h := TestHandler{content: `Repository "proj1" created successfuly`}
@@ -11,5 +23,5 @@ func (s *S) TestNewRepository(c *C) {
 	client := Client{Endpoint: ts.URL}
 	_, err := client.NewRepository("proj1", []string{"someuser"}, false)
 	c.Assert(err, IsNil)
-    c.Assert(string(h.body), Equals, `{"name":"proj1","users":["someuser"],"ispublic":false}`)
+	c.Assert(string(h.body), Equals, `{"name":"proj1","users":["someuser"],"ispublic":false}`)
 }
