f98c2c89c9386234f0dd4a6edefb378de5a346c8
Flavia Missi <flaviamissi@gmail.com>
2012-11-23 10:26:26 -0200

    created NewUser method
    Also created post method and making use of it

--- client.go
+++ client.go
@@ -19,8 +19,13 @@ type repository struct {
 	IsPublic bool     `json:"ispublic"`
 }
 
-func (c *Client) doRequest(method string, body io.Reader) (*http.Response, error) {
-	request, err := http.NewRequest("POST", c.Endpoint, body)
+type user struct {
+	Name string            `json:"name"`
+	Keys map[string]string `json:"keys"`
+}
+
+func (c *Client) doRequest(method, path string, body io.Reader) (*http.Response, error) {
+	request, err := http.NewRequest("POST", c.Endpoint+path, body)
 	if err != nil {
 		return nil, err
 	}
@@ -32,21 +37,36 @@ func (c *Client) doRequest(method string, body io.Reader) (*http.Response, error
 	return response, nil
 }
 
-func (c *Client) NewRepository(name string, users []string, isPublic bool) (repository, error) {
-	r := repository{Name: name, Users: users, IsPublic: isPublic}
-	j, err := json.Marshal(&r)
+func (c *Client) post(i interface{}, path string) error {
+	j, err := json.Marshal(&i)
 	if err != nil {
-		return repository{}, err
+		return err
 	}
 	body := bytes.NewBuffer(j)
-	response, err := c.doRequest("POST", body)
+	response, err := c.doRequest("POST", path, body)
 	if err != nil {
-		return repository{}, err
+		return err
 	}
 	if response.StatusCode != 200 {
 		b, _ := ioutil.ReadAll(response.Body)
-		msg := fmt.Errorf("Got error while performing request. Code: %d - Message: %s", response.StatusCode, b)
-		return repository{}, msg
+		err := fmt.Errorf("Got error while performing request. Code: %d - Message: %s", response.StatusCode, b)
+		return err
+	}
+	return nil
+}
+
+func (c *Client) NewRepository(name string, users []string, isPublic bool) (repository, error) {
+	r := repository{Name: name, Users: users, IsPublic: isPublic}
+	if err := c.post(r, "/repository"); err != nil {
+		return repository{}, err
 	}
 	return r, nil
+}
+
+func (c *Client) NewUser(name string, keys map[string]string) (user, error) {
+	u := user{Name: name, Keys: keys}
+	if err := c.post(u, "/user"); err != nil {
+		return user{}, err
+	}
+	return u, nil
 }
--- client_test.go
+++ client_test.go
@@ -11,10 +11,32 @@ func (s *S) TestDoRequest(c *C) {
 	ts := httptest.NewServer(&h)
 	client := Client{Endpoint: ts.URL}
 	body := bytes.NewBufferString(`{"foo":"bar"}`)
-	response, err := client.doRequest("POST", body)
+	response, err := client.doRequest("POST", "/test", body)
 	c.Assert(err, IsNil)
 	c.Assert(response.StatusCode, Equals, 200)
 	c.Assert(string(h.body), Equals, `{"foo":"bar"}`)
+	c.Assert(h.url, Equals, "/test")
+}
+
+func (s *S) TestPost(c *C) {
+	h := TestHandler{content: `some return message`}
+	ts := httptest.NewServer(&h)
+	client := Client{Endpoint: ts.URL}
+	r := repository{Name: "test", Users: []string{"samwan"}}
+	err := client.post(r, "/repository")
+	c.Assert(err, IsNil)
+	c.Assert(h.url, Equals, "/repository")
+	c.Assert(h.method, Equals, "POST")
+	c.Assert(string(h.body), Equals, `{"name":"test","users":["samwan"],"ispublic":false}`)
+}
+
+func (s *S) TestPostWithError(c *C) {
+	h := ErrorHandler{}
+	ts := httptest.NewServer(&h)
+	client := Client{Endpoint: ts.URL}
+	r := repository{Name: "test", Users: []string{"samwan"}}
+	err := client.post(r, "/repository")
+	c.Assert(err, ErrorMatches, "^Got error while performing request. Code: 400 - Message: Error performing requested operation\n$")
 }
 
 func (s *S) TestNewRepository(c *C) {
@@ -24,6 +46,8 @@ func (s *S) TestNewRepository(c *C) {
 	_, err := client.NewRepository("proj1", []string{"someuser"}, false)
 	c.Assert(err, IsNil)
 	c.Assert(string(h.body), Equals, `{"name":"proj1","users":["someuser"],"ispublic":false}`)
+	c.Assert(h.url, Equals, "/repository")
+	c.Assert(h.method, Equals, "POST")
 }
 
 func (s *S) TestNewRepositoryWithError(c *C) {
@@ -31,6 +55,26 @@ func (s *S) TestNewRepositoryWithError(c *C) {
 	ts := httptest.NewServer(&h)
 	client := Client{Endpoint: ts.URL}
 	_, err := client.NewRepository("proj1", []string{"someuser"}, false)
+	expected := "^Got error while performing request. Code: 400 - Message: Error performing requested operation\n$"
+	c.Assert(err, ErrorMatches, expected)
+}
+
+func (s *S) TestNewUser(c *C) {
+	h := TestHandler{}
+	ts := httptest.NewServer(&h)
+	client := Client{Endpoint: ts.URL}
+	_, err := client.NewUser("someuser", map[string]string{"testkey": "ssh-rsa somekey"})
+	c.Assert(err, IsNil)
+	c.Assert(string(h.body), Equals, `{"name":"someuser","keys":{"testkey":"ssh-rsa somekey"}}`)
+	c.Assert(h.url, Equals, "/user")
+	c.Assert(h.method, Equals, "POST")
+}
+
+func (s *S) TestNewUserWithError(c *C) {
+	h := ErrorHandler{}
+	ts := httptest.NewServer(&h)
+	client := Client{Endpoint: ts.URL}
+	_, err := client.NewUser("someuser", map[string]string{"testkey": "ssh-rsa somekey"})
 	expected := "^Got error while performing request. Code: 400 - Message: Error performing requested operation\n$"
 	c.Assert(err, ErrorMatches, expected)
 }
